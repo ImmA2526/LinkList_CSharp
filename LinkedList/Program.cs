7fe2c8eec9b0a7c0cb338b8c058980a9dae85e94
Avenger2627 <imraninfo97@gmail.com>
2020-12-15 19:28:24 +0530

    [Imran Shaikh] Add: Ability to Append The List

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -10,7 +10,8 @@ namespace LinkedList
             linklist.Add(56);
             linklist.Add(30);
             linklist.Add(70);
-            Console.Write("2nd list Elements Are : ");
+            linklist.Add(120);
+            Console.Write("list Elements Are : ");
             linklist.Display();  
         }
     }
